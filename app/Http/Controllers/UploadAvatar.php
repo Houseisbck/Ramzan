63aa8405aa776e496e14b1c8c2b29b6603119362
Oleg <oleg.vostruxin@yandex.ru>
2021-07-11 12:36:38 +0300

    UploadAvatar

--- app/Http/Controllers/UploadAvatar.php
+++ app/Http/Controllers/UploadAvatar.php
@@ -10,30 +10,23 @@ use Intervention\Image\Facades\Image;
 
 class UploadAvatar extends Controller
 {
-    public function update1Avatar(Request $request)
+    public function updateAvatar(Request $request)
     {
+        $user = Auth::user();
 
-        $user = User::find(Auth::user()->id);
         if ($request->hasFile('avatar')) {
             $avatar = $request->file('avatar');
             $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            if ($user->avatar !== null) {
-                $file = $user->avatar;
-                if (file_exists(storage_path('/app' . $file))) {
-                    unlink(storage_path('/app' . $file));
-                }
-            }
+    
             Image::make($avatar)->resize(1025, 460)->save(storage_path('/app/public/images/' . $filename));
-            $user = Auth::user();
-            $user->avatar = '/public/images/' . $filename;
-            $user->save();
 
-            return response($user);
+            $userAvatar = UserAvatar::create([
+                'user_id' => $user->id,
+                'path' => '/public/images/' . $filename,
+                'path_miniature'
+            ]);
+
+            return response($userAvatar);
         }
-    }
-
-    public function updateAvatar(Request $request)
-    {
-
     }
 }
